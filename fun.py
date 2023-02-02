0cdc995789bbcd8f61adb243ebb182538e8cf5fd
zenoftech <chosentechnologie@gmail.com>
2023-02-02 07:17:36 +0100

    Initial commit

---
+++ fun.py
@@ -0,0 +1,9 @@
+import time
+print("\n","\t"*4,"Created","\n","\t"*3,"\\"*25, "\n","\t"*4,"   By","\n","\t"*4,"Zenoftech\n", "\t"*3,"\\"*25)
+
+for i in range(10):
+    print("Zenoftech is now an ALX student!")
+    time.sleep(1)
+else:
+    print("ALX is so cool!\U0001f618") 
+
